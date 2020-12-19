bf9317e429682bbe14c70560a8a1fdbc6fddddbc
unknown <victor.hugo.pacheco.flores@gmail.com>
2020-12-18 18:59:54 -0600

    Adding querys for exercise 2

--- main.sql
+++ main.sql
@@ -61,6 +61,61 @@ from world
 where capital like concat(name,'_%')
 
 /* --- 2 Select from world---*/
+SELECT name, continent, population FROM world
+
+SELECT name FROM world
+WHERE population >= 200000000
+
+SELECT name, (gdp / population) as per_capita_gdp FROM world
+WHERE population >= 200000000
+
+select name, (population / 1000000) population_in_millions
+from world
+where continent = 'South America'
+
+select name, population
+from world
+where name in('France', 'Germany', 'Italy')
+
+select name
+from world
+where name like '%United%'
+
+select name, population, area
+from world
+where area >= 3000000
+or population >= 250000000
+
+select name, population, area
+from world
+where area >= 3000000
+xor population >= 250000000
+
+select name, round(population/1000000, 2), round(gdp/1000000000, 2)
+from world
+where continent= 'South America'
+
+select name, round(gdp / population, -3)
+from world
+where gdp >= 1000000000000
+
+SELECT name, capital
+  FROM world
+ WHERE LENGTH(name) = LENGTH(capital)
+
+SELECT name, capital
+FROM world
+where left(name, 1) = left(capital, 1)
+and name != capital
+
+SELECT name
+   FROM world
+WHERE name not like '% %'
+and name like '%a%'
+and name like '%e%'
+and name like '%i%'
+and name like '%o%'
+and name like '%u%'
 
 /* --- 3 Select from Nobel---*/
 
