7af1ab7e5a8f88ddcfff1ba6527224092337963a
unknown <victor.hugo.pacheco.flores@gmail.com>
2020-12-21 10:26:23 -0600

    Add section 3

--- main.sql
+++ main.sql
@@ -118,6 +118,64 @@ and name like '%o%'
 and name like '%u%'
 
 /* --- 3 Select from Nobel---*/
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
+select yr, subject
+from nobel
+where winner = 'Albert Einstein'
+
+select winner
+from nobel
+where subject = 'Peace'
+and yr >= 2000
+
+select yr, subject, winner
+from nobel
+where subject = 'Literature'
+and yr between 1980 and 1989
+
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                  'Barack Obama')
+
+select winner from nobel
+where winner like 'John %'
+
+select yr, subject, winner from nobel
+where (yr = 1980 and subject = 'Physics')
+or (yr = 1984 and subject = 'Chemistry')
+
+select yr, subject, winner from nobel
+where yr = 1980
+and subject not in ('Chemistry', 'Medicine')
+
+select yr, subject, winner from nobel
+where (subject = 'Medicine' and yr < 1910)
+or (subject = 'Literature' and yr >= 2004)
+
+select * from nobel
+where winner = 'PETER GRÜNBERG'
+
+select * from nobel
+where winner = "EUGENE O\'NEILL"
+
+select winner, yr, subject from nobel
+where winner like 'Sir%'
+order by yr desc,winner
+
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'),subject,winner
 
 /* --- 4 Select within select---*/
 
